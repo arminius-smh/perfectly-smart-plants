b64c30c3e318789f39115b1b56f8870393f2df4f
arminius-smh <armin@sprejz.de>
2023-11-26 09:34:44 +0100

    add wifi implementation

---
+++ esp32/test/wifi/wifi.ino
@@ -0,0 +1,28 @@
+#include <WiFi.h>
+
+const char *ssid = "ssid";
+const char *password = "password";
+
+void setup() {
+    Serial.begin(115200);
+    connectWIFI();
+}
+
+void loop() {
+    if (WiFi.status() != WL_CONNECTED) {
+        Serial.println("Not connected to WiFi");
+        connectWIFI();
+    }
+}
+
+void connectWIFI() {
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
+
+    Serial.println("Connected to WiFi, IP address: ");
+    Serial.println(WiFi.localIP());
+}
